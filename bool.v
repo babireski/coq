045d620dd8d12ac8b3d6ebaec18f3815d657b186
Elian Chorny Babireski <55952848+babireski@users.noreply.github.com>
2023-04-19 09:34:16 -0300

    Add files via upload

---
+++ bool.v
@@ -0,0 +1,53 @@
+Inductive 𝔹 :=
+	| t
+	| f.
+
+Notation "⊤" := t.
+Notation "⊥" := f.
+
+Definition not (b : 𝔹) : 𝔹 := 
+	match b with
+	| ⊤ => ⊥
+	| ⊥ => ⊥
+	end.
+
+Definition conjunction (b₁ b₂ : 𝔹) : 𝔹 :=
+	match b₁ with
+	| ⊤ => b₂
+	| ⊥ => ⊥
+	end.
+
+Definition disjunction (b₁ b₂ : 𝔹) : 𝔹 :=
+	match b₁ with
+	| ⊤ => ⊤
+	| ⊥ => b₂
+	end.
+
+Definition implication (b₁ b₂ : 𝔹) : 𝔹 :=
+	match b₁ with
+	| ⊤ => b₂
+	| ⊥ => ⊤
+	end.
+
+Notation "¬" := not.
+Infix "∧" := conjunction (at level 80, right  associativity).
+Infix "∨" := disjunction (at level 85, right associativity).
+Infix "→" := implication (at level 99, right associativity).
+
+Lemma demorgan₁ : forall p q : 𝔹, ¬(p ∨ q) = (¬p ∧ ¬q).
+Proof.
+	intros [][].
+		- simpl. reflexivity.
+		- simpl. reflexivity. 
+		- simpl. reflexivity.
+		- simpl. reflexivity.
+Qed.
+
+Lemma demorgan₂ : forall p q : 𝔹, ¬(p ∧ q) = (¬p ∨ ¬q).
+Proof.
+	intros [][].
+		- simpl. reflexivity.
+		- simpl. reflexivity. 
+		- simpl. reflexivity.
+		- simpl. reflexivity.
+Qed.
